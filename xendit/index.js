3de64068c5f90dd34333fe150df2fe9140bf444c
Antoni Angga <antoniangga14@gmail.com>
2017-09-15 14:40:16 +0700

    tambah create_Invoice punya Xendit

--- xendit/index.js
+++ xendit/index.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 function get_Balance (secret_key){
     axios.get('https://api.xendit.co/balance', {
         auth: {
@@ -12,6 +14,29 @@ function get_Balance (secret_key){
     })
 }
 
+function create_Invoice (secret_key,_external_id,_email,_desc,_amount) {
+    axios({
+        method: "post",
+        url: "https://api.xendit.co/v2/invoices",
+        data: {
+            external_id: _external_id,
+            payer_email: _email,
+            description: _desc,
+            amount: _amount
+        },
+        auth: {
+            username: secret_key
+        }
+    })
+    .then(response => {
+        console.log(response.data)
+    })
+    .catch(error => {
+        console.log(error.response.data);
+    })
+}
+
 module.exports = {
-    get_Balance
+    get_Balance,
+    create_Invoice
 }
